d0675dc802d85874e965ba3415d5faf45b09328d
Libernia Waruwu <waruwulibernia@gmail.com>
2025-01-13 16:27:38 +0700

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-// Function to show a specific section
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.section'); 
-    sections.forEach(section => {
-        section.classList.remove('active'); 
-    });
-    document.getElementById(sectionId).classList.add('active'); 
-}
-
-// Function to redirect to WhatsApp
-function orderWhatsApp() {
-    const phone = "6282165493901"; 
-    const message = encodeURIComponent("Halo, saya tertarik untuk membeli Opak Gurih.");
+// Function to redirect to WhatsApp with specific product details
+function orderWhatsApp(namaProduk, harga, deskripsi) {
+    const phone = "6287725323752"; 
+    const message = encodeURIComponent(`Halo, saya tertarik untuk membeli produk berikut:
+    
+Nama Produk: ${namaProduk}
+Harga: ${harga}
+Deskripsi: ${deskripsi}`);
+    
     const url = `https://wa.me/${phone}?text=${message}`;
     window.open(url, '_blank');
 }
